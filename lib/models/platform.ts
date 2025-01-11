754b30d5150d83de471b0538c7afea16d5e00225
Brad Jones <brad@bjc.id.au>
2025-01-11 19:37:47 +1100

    fix(platform): relax input type on suffixExe to make it a bit easier to use in our recipes

--- lib/models/platform.ts
+++ lib/models/platform.ts
@@ -97,8 +97,7 @@ export const currentPlatform: Platform = `${currentOs}-${currentArch}`;
 
 export const splitPlatform = (p: Platform) => p.split("-") as [PlatformOs, PlatformArch];
 
-export const suffixExe = (targetOs: PlatformOs) => (filename: string) =>
-  `${filename}${targetOs === "win" ? ".exe" : ""}`;
+export const suffixExe = (targetOs: string) => (filename: string) => `${filename}${targetOs === "win" ? ".exe" : ""}`;
 
 export function isUnix(v: Platform | PlatformOs) {
   if (v.includes("-")) v = splitPlatform(v as Platform)[0];
