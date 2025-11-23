00530a254acac11a038aaab750326550d0f0de45
Brad Jones <brad@bjc.id.au>
2025-11-24 10:54:10 +1100

    feat(aws-sso-cli): added recipe

---
+++ forge/github.com/synfinatic/aws-sso-cli/recipe.ts
@@ -0,0 +1,42 @@
+import * as r from "lib/mod.ts";
+
+const owner = "synfinatic";
+const repo = "aws-sso-cli";
+
+export default new r.Recipe({
+  name: "aws-sso-cli",
+  version: r.latestGithubTag({ owner, repo }),
+  sources: r.githubReleaseAssets({
+    owner,
+    repo,
+    osMap: { "osx": "darwin", "win": "windows" },
+    archMap: { "64": "amd64" },
+  }),
+  about: {
+    repository: `https://github.com/${owner}/${repo}`,
+    summary: "A powerful tool for using AWS Identity Center for the CLI and web console.",
+    description: await r.http.get("https://raw.githubusercontent.com/synfinatic/aws-sso-cli/refs/heads/main/README.md")
+      .text(),
+    license: "GPL-3.0-only",
+  },
+  build: {
+    dynamic_linking: {
+      binary_relocation: false,
+    },
+    func: async ({ prefixDir, exe, unix }) => {
+      const dst = r.path.join(prefixDir, "bin", exe("aws-sso"));
+      await r.moveGlob("./aws-sso*/aws-sso*", dst);
+      if (unix) await Deno.chmod(dst, 0o755);
+    },
+  },
+  tests: {
+    func: async ({ pkgVersion, exe }) => {
+      const awsVault = r.path.join("bin", exe("aws-sso"));
+      const versionLine = (await r.$`${awsVault} version`.text("combined")).split("\n")[1];
+      const versionText = versionLine.match(/\(v([^\)]+)\)/)?.[1] || "";
+      if (r.coerceSemVer(versionText) !== pkgVersion) {
+        throw new Error(`unexpected version returned from binary`);
+      }
+    },
+  },
+});
