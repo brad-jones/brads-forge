6bdea654ef6c9926cb7cdf6d5be37d849285c345
Brad Jones <brad@bjc.id.au>
2025-11-24 10:07:07 +1100

    feat(aws-vault): added recipe

---
+++ forge/github.com/ByteNess/aws-vault/recipe.ts
@@ -0,0 +1,40 @@
+import * as r from "lib/mod.ts";
+
+const owner = "ByteNess";
+const repo = "aws-vault";
+
+export default new r.Recipe({
+  name: "aws-vault",
+  version: r.latestGithubTag({ owner, repo }),
+  sources: r.githubReleaseAssets({
+    owner,
+    repo,
+    osMap: { "osx": "darwin", "win": "windows" },
+    archMap: { "64": "amd64", "arm64": "aarch64" },
+  }),
+  about: {
+    repository: `https://github.com/${owner}/${repo}`,
+    summary: "A vault for securely storing and accessing AWS credentials in development environments.",
+    description: await r.http.get("https://raw.githubusercontent.com/ByteNess/aws-vault/refs/heads/main/README.md")
+      .text(),
+    license: "MIT",
+  },
+  build: {
+    dynamic_linking: {
+      binary_relocation: false,
+    },
+    func: async ({ prefixDir, exe, unix }) => {
+      const dst = r.path.join(prefixDir, "bin", exe("aws-vault"));
+      await r.moveGlob("./aws-vault*/aws-vault*", dst);
+      if (unix) await Deno.chmod(dst, 0o755);
+    },
+  },
+  tests: {
+    func: async ({ pkgVersion, exe }) => {
+      const awsVault = r.path.join("bin", exe("aws-vault"));
+      if (r.coerceSemVer(await r.$`${awsVault} --version`.text("combined")) !== pkgVersion) {
+        throw new Error(`unexpected version returned from binary`);
+      }
+    },
+  },
+});
