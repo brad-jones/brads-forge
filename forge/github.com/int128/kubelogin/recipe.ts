0b6bff5ffc0cb4f2c75fd06decf5d0142139952a
Brad Jones <brad@bjc.id.au>
2026-05-18 11:37:18 +1000

    fix(kubelogin): check the git sha associated with the tag

--- forge/github.com/int128/kubelogin/recipe.ts
+++ forge/github.com/int128/kubelogin/recipe.ts
@@ -1,4 +1,5 @@
 import * as r from "lib/mod.ts";
+import { Octokit } from "@octokit/rest";
 
 const owner = "int128";
 const repo = "kubelogin";
@@ -14,8 +15,11 @@ export default new r.Recipe({
   }),
   about: {
     homepage: "https://github.com/int128/kubelogin",
-    summary: "About kubectl plugin for Kubernetes OpenID Connect authentication (kubectl oidc-login)",
-    description: await r.http.get("https://raw.githubusercontent.com/int128/kubelogin/refs/heads/master/README.md")
+    summary:
+      "About kubectl plugin for Kubernetes OpenID Connect authentication (kubectl oidc-login)",
+    description: await r.http.get(
+      "https://raw.githubusercontent.com/int128/kubelogin/refs/heads/master/README.md",
+    )
       .text(),
     license: "Apache-2.0",
   },
@@ -28,15 +32,27 @@ export default new r.Recipe({
       const dst = r.path.join(prefixDir, "bin", exe("kubelogin"));
       await r.moveGlob("./kubelogin*/kubelogin*", dst);
       if (unix) await Deno.chmod(dst, 0o755);
-      await r.activation.addLink(dst, r.path.join(prefixDir, "bin", exe("kubectl-oidc_login")));
+      await r.activation.addLink(
+        dst,
+        r.path.join(prefixDir, "bin", exe("kubectl-oidc_login")),
+      );
     },
   },
   tests: {
-    func: async ({ pkgVersion }) => {
-      if (r.coerceSemVer(await r.$`kubelogin --version`.text()) !== pkgVersion) {
+    func: async ({ pkgVersionRaw: tag }) => {
+      const octokit = new Octokit({
+        auth: Deno.env.get("GH_TOKEN") ??
+          Deno.env.get("GITHUB_TOKEN") ??
+          Deno.env.get("GITHUB_API_TOKEN"),
+      });
+      const release = await octokit.repos.getReleaseByTag({ owner, repo, tag });
+      const gitSha = release.data.target_commitish;
+      const expectedVersion = `kubelogin version ${gitSha}`;
+
+      if (await r.$`kubelogin --version`.text() !== expectedVersion) {
         throw new Error(`unexpected version returned from kubelogin`);
       }
-      if (r.coerceSemVer(await r.$`kubectl-oidc_login --version`.text()) !== pkgVersion) {
+      if (await r.$`kubectl-oidc_login --version`.text() !== expectedVersion) {
         throw new Error(`unexpected version returned from kubectl-oidc_login`);
       }
     },
